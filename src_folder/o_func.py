91f81cd073bbe4c959d26ca2a2133804038ca2ef
Jonathan Soltren <91296386+KapitanJon@users.noreply.github.com>
2022-02-18 15:47:25 -0500

    Update o_func.py

--- src_folder/o_func.py
+++ src_folder/o_func.py
@@ -1,17 +1,17 @@
 """Outputs the sum of two ranges"""
-def func_A():
+def func_a():
     """Asks for range input"""
-    a = int(input("Enter the lower limit: "))
-    b = int(input("Enter the upper limit: "))        
+    a_var = int(input("Enter the lower limit: "))
+    b_var = int(input("Enter the upper limit: "))
     total = 0
     for x in range(a, b+1, 1):
         total = total + x
     print(total)
-func_A()
-def func_B():
+func_a()
+def func_b():
     """Asks for exit"""
     f = int(input("Enter a number or 'finish' to stop:"))
     while f:
         print("The square of the entered number is", f**2)
         f = input("Enter a number or 'finish' to stop:")
-funcB()
+func_b()
