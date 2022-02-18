b9bc00d396df95a4e4529e77f505f1eada15caf9
Jonathan Soltren <91296386+KapitanJon@users.noreply.github.com>
2022-02-18 15:48:48 -0500

    Update o_func.py

--- src_folder/o_func.py
+++ src_folder/o_func.py
@@ -4,14 +4,14 @@ def func_a():
     a_var = int(input("Enter the lower limit: "))
     b_var = int(input("Enter the upper limit: "))
     total = 0
-    for x in range(a, b+1, 1):
-        total = total + x
+    for x_var in range(a_var, b_var+1, 1):
+        total = total + x_var
     print(total)
 func_a()
 def func_b():
     """Asks for exit"""
-    f = int(input("Enter a number or 'finish' to stop:"))
+    f_var = int(input("Enter a number or 'finish' to stop:"))
     while f:
         print("The square of the entered number is", f**2)
-        f = input("Enter a number or 'finish' to stop:")
+        f_var = input("Enter a number or 'finish' to stop:")
 func_b()
